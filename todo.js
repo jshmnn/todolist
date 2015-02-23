24b3c1174e989a54af793c6de2844c14419ee275
jshmnn <joshmunyan@gmail.com>
2015-02-23 14:20:48 -0600

    3

--- todo.js
+++ todo.js
@@ -1,10 +1,18 @@
 // alert("Welcome to Ultimate To Do List") 
 //	<li><input type= "checkbox" /> <span> Make this To-Do List </span></li> 
 
-function addNewItem() {
-	alert("Adding new item");
+// Lessons learned
+//- avoid globale variables
+//- strive make functions reusable
+function addNewItem(list) {
+	var listItem = document.createElement("li");
+	listItem.innerText = "Hello";
+
+	list.appendChild(listItem);
 }
 
 var btnNew = document.getElementById("btnAdd");
-btnNew.onclick = addNewItem;
-
+btnNew.onclick = function() {
+	addNewItem(document.getElementById("todolist"));
+	addNewItem(document.getElementById("list2"))
+};
