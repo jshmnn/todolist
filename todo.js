ff2cdae007fb1b820ebe85de0bc80a9a6fe140be
jshmnn <joshmunyan@gmail.com>
2015-02-22 22:56:23 -0600

    first commit

---
+++ todo.js
@@ -0,0 +1,10 @@
+// alert("Welcome to Ultimate To Do List") 
+//	<li><input type= "checkbox" /> <span> Make this To-Do List </span></li> 
+
+function addNewItem() {
+	alert("Adding new item");
+}
+
+var btnNew = document.getElementById("btnAdd");
+btnNew.onclick = addNewItem;
+
