edc5e28c9a19b4060ebea4bd35c25e2ad7e2ff2e
jshmnn <joshmunyan@gmail.com>
2015-02-24 15:35:39 -0600

    tutorial3

--- todo.js
+++ todo.js
@@ -1,25 +1,56 @@
 // alert("Welcome to Ultimate To Do List") 
-//	<li><input type= "checkbox" /> <span> Make this To-Do List </span></li> 
 
 // Lessons learned
 //- avoid globale variables
 //- strive make functions reusable
+function updateItemStatus() {
+	var cbId = this.id.replace("cb_", "");
+	var itemText = document.getElementById("item_"+cbId);
 
-function addNewItem(list, itemText) {
-	var listItem = document.createElement("li");
-	listItem.innerText = itemText;
-
-	list.appendChild(listItem);
+	if (this.checked) {
+		itemText.className = "checked";
+	} 
+	else {
+		itemText.className = "";  
+	}
 }
 
+
+function addNewItem(list, itemText) {
+	totalItems++; 
+
+	var listItem = document.createElement("li");
+
+	var checkbox = document.createElement("input");
+	checkbox.type = "checkbox";
+	checkbox.id = "cb_" + totalItems;
+	checkbox.onclick = updateItemStatus;
+	//here the attribute is TYPE and value is checkbox
+
+	var span = document.createElement("span");
+	span.id = "item_" + totalItems;
+	span.innerText = itemText;
+
+
+	listItem.appendChild(checkbox);
+	listItem.appendChild(span);
+
+	list.appendChild(listItem);
+
+}
+
+var totalItems = 0;
 var inItemText = document.getElementById("inItemText");
 inItemText.focus();
 inItemText.onkeyup = function(event) { 
 
+	//event.which (13) -> ENTER
+	//only proceed if key press is ENTER key
+
 	if (event.which == 13) {
 		var itemText = inItemText.value; 
 
-		if (!itemText || itemText == "" || itemText == " ") {
+		if ( itemText == "" || itemText == " ") {
 				return false;
 		}
 
